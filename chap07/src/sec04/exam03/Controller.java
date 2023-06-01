ed345b959ae2b347adfc919399dcce21c1ad156c
sagong0 <1w2q3e@naver.com>
2023-06-01 18:14:15 +0900

    inherit

---
+++ chap07/src/sec04/exam03/Controller.java
@@ -0,0 +1,21 @@
+//package sec04.exam03;
+//
+//public class Controller {
+//	public MemberService service;
+//	public void setService(MemberService service) {
+//		this.service = service;
+//	}
+//}
+//
+//
+//Controller controller = new Controller();
+//controller.setService(_______________);
+//
+////1) new Service();   (X)
+////2) new memberService(); (O)
+////3) new AService(); // (O)
+////4) new BService(); // (O)
+////5) BoardService(); // (X)
+////6) DService(); // (X)
+//
+//
---
+++ chap07/src/sec04/exam04/ServiceExample.java
@@ -0,0 +1,13 @@
+package sec04.exam04;
+
+public class ServiceExample {
+	public static void main(String[] args) {
+		Controller controller = new Controller();
+//		controller.setService(new Service());
+//		controller.service.login(); >> controller안에 login 이란거 자체가 없음
+		controller.setService(new MemberService());
+		controller.service.login();
+		controller.setService(new AService());
+		controller.service.login();
+	}
+}
